5916bf61ca066f3587bab29a4371b19b230b1227
Kashirin Maxim <ksh.max@gmail.com>
2017-10-12 11:16:30 +0300

    Initial

---
+++ android/dtcore/src/main/java/com/kvteam/deliverytracker/core/TestType.kt
@@ -0,0 +1,8 @@
+package com.kvteam.deliverytracker.core
+
+/**
+ * Created by maxim on 13.09.17.
+ */
+class TestType {
+    fun TestFun() = "Hello, Core"
+}
---
+++ android/performerapp/src/main/java/com/kvteam/deliverytracker/performerapp/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.kvteam.deliverytracker.performerapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kvteam.deliverytracker.core.TestType
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val a = TestType()
+    }
+}
