a5bfd0aa187b65bd5f14a8526dde6d9aaa31b76c
Kashirin Maxim <ksh.max@gmail.com>
2018-01-29 23:12:13 +0300

    Действие над элементом коллекции в справочнике

--- android/dtcore/src/main/java/com/kvteam/deliverytracker/core/models/ReferenceCollectionBase.kt
+++ android/dtcore/src/main/java/com/kvteam/deliverytracker/core/models/ReferenceCollectionBase.kt
@@ -1,13 +1,14 @@
 package com.kvteam.deliverytracker.core.models
 
 import com.google.gson.annotations.SerializedName
-import com.google.gson.internal.LinkedTreeMap
 import java.util.*
 
 open class ReferenceCollectionBase(
         id: UUID? = null,
         instanceId: UUID? = null,
-        @SerializedName("ParentId") var parentId: UUID? = null) : ReferenceEntityBase(id, instanceId) {
+        @SerializedName("ParentId") var parentId: UUID? = null,
+        @SerializedName("Action") var action: CollectionEntityAction = CollectionEntityAction.None) :
+            ReferenceEntityBase(id, instanceId) {
     override fun fromMap(map: Map<*, *>) {
         super.fromMap(map)
         val parentIdStr = map["ParentId"] as? String
