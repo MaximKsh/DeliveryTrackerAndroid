bd9f743ac57e686f947edfde5f56eabbe00d3db4
Kashirin Maxim <ksh.max@gmail.com>
2018-03-30 14:59:05 +0300

    Small fixes

--- android/dtcore/src/main/java/com/kvteam/deliverytracker/core/tasks/TaskExtensions.kt
+++ android/dtcore/src/main/java/com/kvteam/deliverytracker/core/tasks/TaskExtensions.kt
@@ -1,13 +1,33 @@
 package com.kvteam.deliverytracker.core.tasks
 
-fun String.toTaskState(): TaskState? {
-    return when(this){
-        TaskState.NewUndistributed.simpleName -> TaskState.NewUndistributed
-        TaskState.New.simpleName -> TaskState.New
-        TaskState.InWork.simpleName -> TaskState.InWork
-        TaskState.Performed.simpleName -> TaskState.Performed
-        TaskState.Cancelled.simpleName -> TaskState.Cancelled
-        TaskState.CancelledByManager.simpleName -> TaskState.CancelledByManager
+import com.kvteam.deliverytracker.core.common.EMPTY_STRING
+import com.kvteam.deliverytracker.core.common.ILocalizationManager
+import com.kvteam.deliverytracker.core.models.TaskInfo
+import java.util.*
+
+fun getTaskStateById(id: UUID?) : TaskState? {
+    return when(id){
+        TaskState.Preparing.id -> TaskState.Preparing
+        TaskState.Queue.id -> TaskState.Queue
+        TaskState.Waiting.id -> TaskState.Waiting
+        TaskState.IntoWork.id -> TaskState.IntoWork
+        TaskState.Delivered.id -> TaskState.Delivered
+        TaskState.Complete.id -> TaskState.Complete
+        TaskState.Revoked.id -> TaskState.Revoked
         else -> null
     }
 }
+
+fun UUID.getTaskState(): TaskState? {
+    return getTaskStateById(this)
+}
+
+fun TaskInfo.getTaskState(): TaskState? {
+    return getTaskStateById(this.taskStateId)
+}
+
+fun TaskInfo.getTaskStateCaption(lm: ILocalizationManager) : String {
+    val taskState = this.getTaskState() ?: return EMPTY_STRING
+    return lm.getString(taskState.stateCaption)
+}
+
--- android/dtcore/src/main/java/com/kvteam/deliverytracker/core/tasks/TaskState.kt
+++ android/dtcore/src/main/java/com/kvteam/deliverytracker/core/tasks/TaskState.kt
@@ -1,46 +1,60 @@
 package com.kvteam.deliverytracker.core.tasks
 
-import com.kvteam.deliverytracker.core.R
+import java.util.*
 
 enum class TaskState {
-    NewUndistributed {
-        override val simpleName = "TaskState_NewUndistributed"
-        override val localizationStringId = R.string.Core_TaskState_NewUndistributed
-        override val localizationStringName = "Core_TaskState_NewUndistributed"
+    Preparing {
+        override val id: UUID = UUID.fromString("8c9c1011-f7c1-4cef-902f-4925f5e83f4a")
+        override val stateName: String = "Preparing"
+        override val stateCaption: String = "ServerMessage_TaskStates_Preparing"
+        override val color: Int = 0
     },
 
-    New {
-        override val simpleName = "TaskState_New"
-        override val localizationStringId = R.string.Core_TaskState_New
-        override val localizationStringName = "Core_TaskState_New"
+    Queue {
+        override val id: UUID = UUID.fromString("d4595da3-6a5f-4455-b975-7637ea429cb5")
+        override val stateName: String = "Queue"
+        override val stateCaption: String = "ServerMessage_TaskStates_Queue"
+        override val color: Int = 0
     },
 
-    InWork {
-        override val simpleName = "TaskState_InWork"
-        override val localizationStringId = R.string.Core_TaskState_InWork
-        override val localizationStringName = "Core_TaskState_InWork"
+    Waiting {
+        override val id: UUID = UUID.fromString("0a79703f-4570-4a58-8509-9e598b1eefaf")
+        override val stateName: String = "Waiting"
+        override val stateCaption: String = "ServerMessage_TaskStates_Waiting"
+        override val color: Int = 0
     },
 
-    Performed {
-        override val simpleName = "TaskState_Performed"
-        override val localizationStringId = R.string.Core_TaskState_Performed
-        override val localizationStringName = "Core_TaskState_Performed"
+    IntoWork {
+        override val id: UUID = UUID.fromString("8912d18f-192a-4327-bd47-5c9963b5f2b0")
+        override val stateName: String = "IntoWork"
+        override val stateCaption: String = "ServerMessage_TaskStates_IntoWork"
+        override val color: Int = 0
     },
 
-    Cancelled {
-        override val simpleName = "TaskState_Cancelled"
-        override val localizationStringId = R.string.Core_TaskState_Cancelled
-        override val localizationStringName = "Core_TaskState_Cancelled"
+    Delivered {
+        override val id: UUID = UUID.fromString("020d7c7e-bb4e-4add-8b11-62a91471b7c8")
+        override val stateName: String = "Delivered"
+        override val stateCaption: String = "ServerMessage_TaskStates_Delivered"
+        override val color: Int = 0
     },
 
-    CancelledByManager {
-        override val simpleName = "TaskState_CancelledByManager"
-        override val localizationStringId = R.string.Core_TaskState_CancelledByManager
-        override val localizationStringName = "Core_TaskState_CancelledByManager"
+    Complete {
+        override val id: UUID = UUID.fromString("d91856f9-d1bf-4fad-a46e-c3baafabf762")
+        override val stateName: String = "Complete"
+        override val stateCaption: String = "ServerMessage_TaskStates_Complete"
+        override val color: Int = 0
+    },
+
+    Revoked {
+        override val id: UUID = UUID.fromString("d2e70369-3d37-420f-b176-5fa0b2c1d4a9")
+        override val stateName: String = "Revoked"
+        override val stateCaption: String = "ServerMessage_TaskStates_Revoked"
+        override val color: Int = 0
     }
     ;
 
-    abstract val simpleName: String
-    abstract val localizationStringId: Int
-    abstract val localizationStringName: String
+    abstract val id: UUID
+    abstract val stateName: String
+    abstract val stateCaption: String
+    abstract val color: Int
 }
